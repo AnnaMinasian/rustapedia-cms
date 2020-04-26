70c7a51d8ca5b86a1ff4cd6ed7c6338566f5894c
Anna Minasian <32515945+AnnaMinasian@users.noreply.github.com>
2020-04-26 23:38:09 +0300

    Add some change to CraftSchema

--- lists/Craft.js
+++ lists/Craft.js
@@ -2,7 +2,10 @@ const { Text, Relationship } = require('@keystonejs/fields');
 
 module.exports = {
   fields: {
-      requiredItemCounts: { type: Relationship, ref: 'ItemCount', many: true },
-      time: { type: Text },
+    blueprint: { type: Text },
+    requiredItemCounts: { type: Relationship, ref: 'ItemCount', many: true },
+    time: { type: Text },
+    item: { type: Relationship, ref: 'Item.craftInfo' },
+    workBench: { type: Relationship, ref: 'Item.usedForCraft' }
   }
 };
