f49ae5a3f0bd994ee1f14a1fe0bb80bcdf8f1525
AnnaMinasian <minasianann@gmail.com>
2020-05-18 18:28:27 +0300

    Add changes to the ItemSchema

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-const { Keystone } = require('@keystonejs/keystone');
-const { GraphQLApp } = require('@keystonejs/app-graphql');
-const { AdminUIApp } = require('@keystonejs/app-admin-ui');
-const { MongooseAdapter: Adapter } = require('@keystonejs/adapter-mongoose');
-require('dotenv').config();
-const { PasswordAuthStrategy } = require('@keystonejs/auth-password');
+const { Keystone } = require("@keystonejs/keystone");
+const { GraphQLApp } = require("@keystonejs/app-graphql");
+const { AdminUIApp } = require("@keystonejs/app-admin-ui");
+const { MongooseAdapter: Adapter } = require("@keystonejs/adapter-mongoose");
+require("dotenv").config();
+const { PasswordAuthStrategy } = require("@keystonejs/auth-password");
 const PROJECT_NAME = "rustapedia";
-const UserSchema = require('./lists/User.js');
-const ItemSchema = require('./lists/Item.js');
-const SubCategorySchema = require('./lists/SubCategory.js');
-const LootInfoSchema = require('./lists/LootInfo.js');
-const MainInfoSchema = require('./lists/MainInfo.js');
-const WeaponInfoSchema = require('./lists/WeaponInfo.js');
-const FoodInfoSchema = require('./lists/FoodInfo.js');
-const CraftSchema = require('./lists/Craft.js');
-const ResearchInfoSchema = require('./lists/ResearchInfo.js');
-const ItemCountSchema = require('./lists/ItemCount.js');
-const ExperimentSchema = require('./lists/Experiment.js');
-const RepairSchema = require('./lists/Repair.js');
-const RecycleSchema = require('./lists/Recycle.js');
-const DurabilityInfoSchema = require('./lists/DurabilityInfo.js');
-const CookingInfoSchema = require('./lists/CookingInfo.js');
-const IngredientInfoSchema = require('./lists/IngredientInfo.js');
-const GatheringInfoSchema = require('./lists/GatheringInfo.js');
-const DamageInfoSchema = require('./lists/DamageInfo.js');
-const CategorySchema = require('./lists/Category.js');
-const ItemInfoSchema = require('./lists/ItemInfo.js');
+const UserSchema = require("./lists/User.js");
+const ItemSchema = require("./lists/Item.js");
+const SubCategorySchema = require("./lists/SubCategory.js");
+const LootInfoSchema = require("./lists/LootInfo.js");
+const MainInfoSchema = require("./lists/MainInfo.js");
+const WeaponInfoSchema = require("./lists/WeaponInfo.js");
+const FoodInfoSchema = require("./lists/FoodInfo.js");
+const CraftSchema = require("./lists/Craft.js");
+const ResearchInfoSchema = require("./lists/ResearchInfo.js");
+const ItemCountSchema = require("./lists/ItemCount.js");
+const ExperimentSchema = require("./lists/Experiment.js");
+const RepairSchema = require("./lists/Repair.js");
+const RecycleSchema = require("./lists/Recycle.js");
+const DurabilityInfoSchema = require("./lists/DurabilityInfo.js");
+const CookingInfoSchema = require("./lists/CookingInfo.js");
+const IngredientInfoSchema = require("./lists/IngredientInfo.js");
+const GatheringInfoSchema = require("./lists/GatheringInfo.js");
+const DamageInfoSchema = require("./lists/DamageInfo.js");
+const CategorySchema = require("./lists/Category.js");
+const ItemInfoSchema = require("./lists/ItemInfo.js");
 
 /**
  * You've got a new KeystoneJS Project! Things you might want to do next:
@@ -38,39 +38,41 @@ const keystone = new Keystone({
   cookie: {
     secure: true,
   },
-  cookieSecret: process.env.COOKIE_SECRET
+  cookieSecret: process.env.COOKIE_SECRET,
 });
-keystone.createList('User', UserSchema);
-keystone.createList('Item', ItemSchema);
-keystone.createList('SubCategory', SubCategorySchema);
-keystone.createList('LootInfo', LootInfoSchema);
-keystone.createList('MainInfo', MainInfoSchema);
-keystone.createList('FoodInfo', FoodInfoSchema);
-keystone.createList('WeaponInfo', WeaponInfoSchema);
-keystone.createList('Recycle', RecycleSchema);
-keystone.createList('Craft', CraftSchema);
-keystone.createList('Experiment', ExperimentSchema);
-keystone.createList('ItemCount', ItemCountSchema);
-keystone.createList('ResearchInfo', ResearchInfoSchema);
-keystone.createList('Repair', RepairSchema);
-keystone.createList('DurabilityInfo', DurabilityInfoSchema);
-keystone.createList('CookingInfo', CookingInfoSchema);
-keystone.createList('IngredientInfo', IngredientInfoSchema);
-keystone.createList('GatheringInfo', GatheringInfoSchema);
-keystone.createList('DamageInfo', DamageInfoSchema);
-keystone.createList('Category', CategorySchema);
-keystone.createList('ItemInfo', ItemInfoSchema);
-
+keystone.createList("User", UserSchema);
+keystone.createList("Item", ItemSchema);
+keystone.createList("SubCategory", SubCategorySchema);
+keystone.createList("LootInfo", LootInfoSchema);
+keystone.createList("MainInfo", MainInfoSchema);
+keystone.createList("FoodInfo", FoodInfoSchema);
+keystone.createList("WeaponInfo", WeaponInfoSchema);
+keystone.createList("Recycle", RecycleSchema);
+keystone.createList("Craft", CraftSchema);
+keystone.createList("Experiment", ExperimentSchema);
+keystone.createList("ItemCount", ItemCountSchema);
+keystone.createList("ResearchInfo", ResearchInfoSchema);
+keystone.createList("Repair", RepairSchema);
+keystone.createList("DurabilityInfo", DurabilityInfoSchema);
+keystone.createList("CookingInfo", CookingInfoSchema);
+keystone.createList("IngredientInfo", IngredientInfoSchema);
+keystone.createList("GatheringInfo", GatheringInfoSchema);
+keystone.createList("DamageInfo", DamageInfoSchema);
+keystone.createList("Category", CategorySchema);
+keystone.createList("ItemInfo", ItemInfoSchema);
 
 const authStrategy = keystone.createAuthStrategy({
   type: PasswordAuthStrategy,
-  list: 'User',
+  list: "User",
 });
 
 module.exports = {
   keystone,
-  apps: [new GraphQLApp(), new AdminUIApp({ enableDefaultRoute: true, authStrategy })],
-  configureExpress: app => {
-    app.set('trust proxy', 1);
-  }
+  apps: [
+    new GraphQLApp(),
+    new AdminUIApp({ enableDefaultRoute: true, authStrategy }),
+  ],
+  configureExpress: (app) => {
+    app.set("trust proxy", 1);
+  },
 };
--- lists/Item.js
+++ lists/Item.js
@@ -1,44 +1,62 @@
-const { Text, Integer, Relationship, CloudinaryImage, Select } = require('@keystonejs/fields');
-const { CloudinaryAdapter } = require('@keystonejs/file-adapters');
+const {
+  Text,
+  Integer,
+  Relationship,
+  CloudinaryImage,
+  Select,
+} = require("@keystonejs/fields");
+const { CloudinaryAdapter } = require("@keystonejs/file-adapters");
 
 const cloudinaryAdapter = new CloudinaryAdapter({
-    cloudName: process.env.CLOUDINARY_CLOUD_NAME,
-    apiKey: process.env.CLOUDINARY_KEY,
-    apiSecret: process.env.CLOUDINARY_SECRET,
-    folder: 'images',
-  });
-  
+  cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+  apiKey: process.env.CLOUDINARY_KEY,
+  apiSecret: process.env.CLOUDINARY_SECRET,
+  folder: "images",
+});
+
 module.exports = {
-    fields: {
-    category: { type: Relationship, ref: 'Category' },
-    subCategory: {type: Relationship, ref: 'SubCategory.items' },
+  fields: {
+    category: { type: Relationship, ref: "Category" },
+    subCategory: { type: Relationship, ref: "SubCategory.items" },
     name: { type: Text, isRequired: true },
     image: { type: CloudinaryImage, adapter: cloudinaryAdapter },
     text: { type: Text },
     additionalText: { type: Text },
     blueprint: { type: Text },
-    itemInfo: { type: Relationship, ref: 'ItemInfo'},
-    mainInfo: { type: Relationship, ref: 'MainInfo' },
-    weaponInfo: { type: Relationship, ref: 'WeaponInfo' },
-    foodInfo: { type: Relationship, ref: 'FoodInfo' },
-    lootInfo: { type: Relationship, ref:'LootInfo.itemToLoot', many: true },
-    loot: { type: Relationship, ref:'LootInfo.container', many: true },
-    craftInfo: { type: Relationship, ref: 'Craft.item' },
-    usedForCraft: { type: Relationship, ref: 'Craft.workBench', many: true },
-    ingredientFor: { type: Relationship, ref: 'Craft.ingredients', many: true },
-    experiment: { type: Relationship, ref: 'Experiment.item' },
-    experimentation: { type: Relationship, ref: 'Experiment.workBench', many: true },
-    research: { type: Relationship, ref: 'ResearchInfo.item' },
-    researches: { type: Relationship, ref: 'ResearchInfo.researchTool', many: true }, 
-    repair: { type: Relationship, ref: 'Repair.item', many: true },
-    repairs: { type: Relationship, ref: 'Repair.tool', many: true },
-    recycle: { type: Relationship, ref: 'Recycle.item' },
-    recycledFrom: { type: Relationship, ref: 'Recycle.recycledFrom', many: true },
-    recycler: { type: Relationship, ref: 'Recycle.recycler', many: true },
-    durabilityInfo: { type: Relationship, ref: 'DurabilityInfo', many: true },
-    
-    cookingInfo: { type: Relationship, ref: 'CookingInfo', many: true },
-    gatheringInfo: { type: Relationship, ref: 'GatheringInfo', many: true },
-    damageInfo: { type: Relationship, ref: 'DamageInfo', many: true },
+    itemInfo: { type: Relationship, ref: "ItemInfo" },
+    mainInfo: { type: Relationship, ref: "MainInfo" },
+    weaponInfo: { type: Relationship, ref: "WeaponInfo" },
+    foodInfo: { type: Relationship, ref: "FoodInfo" },
+    lootInfo: { type: Relationship, ref: "LootInfo.itemToLoot", many: true },
+    loot: { type: Relationship, ref: "LootInfo.container", many: true },
+    craftInfo: { type: Relationship, ref: "Craft.item" },
+    usedForCraft: { type: Relationship, ref: "Craft.workBench", many: true },
+    ingredientFor: { type: Relationship, ref: "Craft.ingredients", many: true },
+    experiment: { type: Relationship, ref: "Experiment.item" },
+    experimentation: {
+      type: Relationship,
+      ref: "Experiment.workBench",
+      many: true,
     },
+    research: { type: Relationship, ref: "ResearchInfo.item" },
+    researches: {
+      type: Relationship,
+      ref: "ResearchInfo.researchTool",
+      many: true,
+    },
+    repair: { type: Relationship, ref: "Repair.item", many: true },
+    repairs: { type: Relationship, ref: "Repair.tool", many: true },
+    recycle: { type: Relationship, ref: "Recycle.item" },
+    recycledFrom: {
+      type: Relationship,
+      ref: "Recycle.recycledFrom",
+      many: true,
+    },
+    recycler: { type: Relationship, ref: "Recycle.recycler", many: true },
+    durabilityInfo: { type: Relationship, ref: "DurabilityInfo", many: true },
+
+    cookingInfo: { type: Relationship, ref: "CookingInfo", many: true },
+    gatheringInfo: { type: Relationship, ref: "GatheringInfo", many: true },
+    damageInfo: { type: Relationship, ref: "DamageInfo", many: true },
+  },
 };
