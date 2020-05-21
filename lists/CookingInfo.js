390d76ff31a9636828e08275d8923fb0867f147c
Anna Minasian <32515945+AnnaMinasian@users.noreply.github.com>
2020-05-21 14:49:12 +0300

    Add some change to CookingInfo

--- lists/CookingInfo.js
+++ lists/CookingInfo.js
@@ -2,8 +2,8 @@ const { Relationship } = require('@keystonejs/fields');
 
 module.exports = {
   fields: {
-    item: { type: Relationship, ref: 'Item.cookingInfo'},
-    furnace: { type: Relationship, ref: 'Item.cooking' },
+    item: { type: Relationship, ref: 'Item.cookingInfo', many: true },
+    furnace: { type: Relationship, ref: 'Item.cooking', many: true },
     processOne: { type: Relationship, ref: 'IngredientInfo'},
     processTwo: { type: Relationship, ref: 'IngredientInfo'},
     processThree: { type: Relationship, ref: 'IngredientInfo'},
