a89ba1f5acaace53520c22325ec65fb7fe6942de
AnnaMinasian <minasianann@gmail.com>
2020-06-23 17:00:21 +0300

    Add some changes to Schema

--- lists/DurabilityInfo.js
+++ lists/DurabilityInfo.js
@@ -5,6 +5,8 @@ module.exports = {
     type: { type: Select, options: "Explosive, Melle, Guns, Throwing" },
     item: { type: Relationship, ref: "Item" },
     ammo: { type: Relationship, ref: "Item" },
+    weapon: { type: Relationship, ref: "Item" },
+    text: { type: Text },
     time: { type: Text },
     fuelNeeded: { type: Integer },
     sulfurNeeded: { type: Integer },
--- lists/ItemInfo.js
+++ lists/ItemInfo.js
@@ -1,10 +1,12 @@
-const { Integer, Text } = require('@keystonejs/fields');
+const { Integer, Text } = require("@keystonejs/fields");
 
 module.exports = {
   fields: {
     respawnTimer: { type: Integer },
-    decay: { type:  Integer },
+    decay: { type: Integer },
+    decayTimeOutside: { type: Integer },
+    decayTimeInside: { type: Integer },
     upkeep: { type: Integer },
     speed: { type: Text },
-  }
+  },
 };
