068ba90da5ccc8647d81ef7d2a552a9862d6edd9
AnnaMinasian <minasianann@gmail.com>
2020-07-17 11:42:07 +0300

    Add field in CraftInfoSchema

--- lists/Craft.js
+++ lists/Craft.js
@@ -1,11 +1,12 @@
-const { Text, Relationship } = require('@keystonejs/fields');
+const { Text, Relationship, Integer } = require("@keystonejs/fields");
 
 module.exports = {
   fields: {
-    requiredItemCounts: { type: Relationship, ref: 'ItemCount', many: true },
-    ingredients: { type: Relationship, ref: 'Item.ingredientFor', many: true }, 
+    requiredItemCounts: { type: Relationship, ref: "ItemCount", many: true },
+    ingredients: { type: Relationship, ref: "Item.ingredientFor", many: true },
+    amount: { type: Integer },
     time: { type: Text },
-    item: { type: Relationship, ref: 'Item.craftInfo' },
-    workBench: { type: Relationship, ref: 'Item.usedForCraft' },
-  }
+    item: { type: Relationship, ref: "Item.craftInfo" },
+    workBench: { type: Relationship, ref: "Item.usedForCraft" },
+  },
 };
