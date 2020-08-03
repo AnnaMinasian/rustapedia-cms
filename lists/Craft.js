b082c9530d57af2338daa18572e7cc64c077dfbe
AnnaMinasian <minasianann@gmail.com>
2020-08-03 19:49:25 +0300

    Add some changes to CraftSchema

--- lists/Craft.js
+++ lists/Craft.js
@@ -1,12 +1,15 @@
-const { Text, Relationship, Integer } = require("@keystonejs/fields");
+const { Text, Select, Relationship, Integer } = require("@keystonejs/fields");
 
 module.exports = {
   fields: {
+    item: { type: Relationship, ref: "Item.craftInfo" },
+    workBenchNeeded: { type: Select, options: "Yes, No" },
     requiredItemCounts: { type: Relationship, ref: "ItemCount", many: true },
     ingredients: { type: Relationship, ref: "Item.ingredientFor", many: true },
     amount: { type: Integer },
-    time: { type: Text },
-    item: { type: Relationship, ref: "Item.craftInfo" },
-    workBench: { type: Relationship, ref: "Item.usedForCraft" },
+    workBench: { type: Relationship, ref: "Item.usedForCraft", many: true },
+    time1: { type: Text },
+    time2: { type: Text },
+    time3: { type: Text },
   },
 };
